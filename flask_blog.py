583020cac6d40cb433454ab9de092f06b754edc4
Dan Gutt <dgutt10@gmail.com>
2020-12-14 09:34:58 -0600

    ORM not quit working yet.

--- flask_blog.py
+++ flask_blog.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 
 app = Flask(__name__)
-app.config['SQLAlCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432' 
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'postgresql:///flask_blog_dev' 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +19,7 @@ class User(db.Model):
   email = db.Column(db.String(120), unique=True, nullable=False)
   image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
   password = db.Column(db.String(60), nullable=False)
+  posts = db.relationship('Post', backref='author', lazy=True)
 
   def __repr__(self):
     return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -21,9 +29,11 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('Author')
 
     def __repr__(self):
-    return f"Post('{self.title}', '{self.date_posted}', '{self.content}')"
+      return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
   {
