6d91138b722ffec0a17f7c0fd609f3e2268d0290
Dan Gutt <dgutt10@gmail.com>
2020-12-15 09:37:31 -0600

    Had to remove the relationship methods to be able to save the user and the posts.

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ class User(db.Model):
   email = db.Column(db.String(120), unique=True, nullable=False)
   image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
   password = db.Column(db.String(60), nullable=False)
-  posts = db.relationship('Post', backref='author', lazy=True)
 
   def __repr__(self):
     return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -26,7 +25,6 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    author = db.relationship('Author')
 
     def __repr__(self):
       return f"Post('{self.title}', '{self.date_posted}')"
